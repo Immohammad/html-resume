45240c4b267a7c6228b01881b8396cf7d485e00a
Mohammad <mo.movahedinia@gmail.com>
2023-01-30 10:48:05 +0330

    last commit

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
-function showContact() {}
-function closeContact() {}
+function showContact() {
+  document.getElementById("contactOverlay").style.display = "block";
+}
+function closeContact() {
+  document.getElementById("contactOverlay").style.display = "none";
+  document.getElementById("skillsOverlay").style.display = "none";
+}
+function stop(event) {
+  event.stopPropagation();
+}
+function moreSkills() {
+    document.getElementById("skillsOverlay").style.display = "block";
+}
